e383e2d4a33858b926981d5d1ae03a7ed7853855
João Paulo Arantes <jparantes.racing@gmail.com>
2023-07-31 01:42:36 +0000

    Aula de declaracao

---
+++ src/main/java/com/example/AulaDeclaracao.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class AulaDeclaracao {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Minha aplicação Java\n");
+        // tipo NomeDaVariavel;
+        int idade = 10;
+        double peso = 80.5;
+        System.out.println("Idade é " + idade + " e peso é " + peso);
+    }
+}
