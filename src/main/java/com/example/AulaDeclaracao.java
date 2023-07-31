6a231e80058520f6766036734f32e4838e84f720
João Paulo Arantes <jparantes.racing@gmail.com>
2023-07-31 02:23:32 +0000

    Continuação aula de declaração

--- src/main/java/com/example/AulaDeclaracao.java
+++ src/main/java/com/example/AulaDeclaracao.java
@@ -7,9 +7,18 @@ public class AulaDeclaracao {
         System.out.println();
         System.out.println();
         System.out.println("Minha aplicação Java\n");
-        // tipo NomeDaVariavel;
+        // tipo NomeDaVariavel = valor;
         int idade = 10;
         double peso = 80.5;
         System.out.println("Idade é " + idade + " e peso é " + peso);
+
+        char sexo = 'M';
+        System.out.println("Sexo é " + sexo);
+
+        // Em Java 1 e 0 não é aceito como true ou false, JavaScript já aceita
+        boolean NecessidadesEspeciais = false;
+        System.out.println("Possui necessidades especiais: " + NecessidadesEspeciais);
+
+        //Usar var NomeDaVariavel funciona a partir do Java 10, pois o compilador detecta o tipo de variável automaticamente
     }
 }
