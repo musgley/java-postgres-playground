3b5f71a47b87099ef8ada106b99fda956a59142e
João Paulo Arantes <jparantes.racing@gmail.com>
2023-07-31 14:48:17 +0000

    For de arrays

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -17,7 +17,34 @@ public class AppArrays {
         matriz[0][1] = -20; //Trocando o valor da linha 0 e coluna 1
         System.out.println(Arrays.toString(matriz[0]));
         System.out.println(Arrays.toString(matriz[1])); //Usar somente Arrays.toString(matriz) não imprime por estaria tentando imprimir o vetor
-        matriz[0] = new double[]{1.0, 2.0, 3.0}; //Posso declarar a linha inteira de uma vez, visto que a linha é um vetor. Lembrando tb que se for double preciso 1.0 e não 1
+        matriz[0] = new double[]{1, 2, 3}; //Posso declarar a linha inteira de uma vez, visto que a linha é um vetor
         System.out.println(Arrays.toString(matriz[0]));
+        System.out.println();
+
+        //Definindo valores do vetor usando um for
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = 100*(i+1);
+        }
+        System.out.println(Arrays.toString(vetor));
+        System.out.println();
+
+        double matriznova[][] = new double[3][4];
+        //Definindo valores da matriz usando um for
+        for (int i = 0; i < matriznova.length; i++) { //matriz.length retorna número de linhas da matriz
+            for (int j = 0; j < matriznova[i].length; j++) { //matriz[i].length retorna o número de colunas da linha i
+                matriznova[i][j] = 30*(i+j+1);
+            }
+        }
+        System.out.println(Arrays.toString(matriznova[0]));
+        System.out.println(Arrays.toString(matriznova[1]));
+        System.out.println(Arrays.toString(matriznova[2]));
+        System.out.println();
+
+        for (int i = 0; i < matriznova.length; i++) {
+            for (int j = 0; j < matriznova[i].length; j++) {
+                System.out.printf("%5.1f ", matriznova[i][j]); //printf imprime o inteiro, sem espaço ao final da linha. O %5.1f representa o float com 5 caracteres e 1 casa decimal
+            }
+            System.out.println();
+        } //Impressão em formato de matriz e alinhado
     }
 }
