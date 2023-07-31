edecf64f4995e493eb19e0f325fb6d79f6721867
João Paulo Arantes <jparantes.racing@gmail.com>
2023-07-31 01:13:46 +0000

    Alteração AppTest.java

--- src/main/java/com/example/AppTest.java
+++ src/main/java/com/example/AppTest.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class AppTest {
     public static void main(String[] args) {
-        system.out.println("App de teste");
+        System.out.println("App de teste");
     }
 }
