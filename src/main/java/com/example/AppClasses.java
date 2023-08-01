fc22155fcf423e7e47b27cc83b2f015048dbb569
João Paulo Arantes <jparantes.racing@gmail.com>
2023-08-01 21:11:28 +0000

    Aula de Strings

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -7,6 +7,10 @@ public class AppClasses {
         cliente.setRenda(10000);
         cliente.setSexo('M');
         cliente.setAnoNascimento(1980);
+        cliente.setNome("           Manoel");
+        cliente.setCpf("99999999999");
+        cliente.setCidade("Palmas");
+        System.out.println("Nome: " + cliente.getNome());
         System.out.println("Renda: " + cliente.getRenda());
         System.out.println("Sexo: " + cliente.getSexo());
         System.out.println("Ano de nascimento: " + cliente.getAnoNascimento());
@@ -15,6 +19,21 @@ public class AppClasses {
 
         Cliente cliente2 = new Cliente(5000, 'F');
         cliente2.setAnoNascimento(1988);
+        cliente2.setNome("Joana");
+        cliente2.setCpf("99999999999");
+        cliente2.setCidade("PALMAS");
+
+        if(cliente.getCpf().equals(cliente2.getCpf()))
+            System.out.println("Cliente 1 e Cliente 2 possuem o mesmo CPF.");
+        else {
+            System.out.println("Os clientes possuem CPF diferentes.");
+        }
+        if(cliente.getCidade().equalsIgnoreCase(cliente2.getCidade())) //equalsIgnoreCase testa igualdade de strings ignorando se maiúsculas ou minúsculas
+            System.out.println("Cliente 1 e Cliente 2 moram na mesma cidade.");
+        else {
+            System.out.println("Os clientes moram em cidades diferentes.");
+        }
+        System.out.println("Nome: " + cliente2.getNome());
         System.out.println("Renda: " + cliente2.getRenda());
         System.out.println("Sexo: " + cliente2.getSexo());
         System.out.println("Ano de nascimento: " + cliente2.getAnoNascimento());
--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -1,17 +1,14 @@
 package com.example;
 
 public class Cliente {
+    private String cpf;
+    private String nome;
+    private String cidade;
     private double renda;
     private char sexo;
-    private boolean especial ;
+    private boolean especial;
+    private int anoNascimento;
     
-    public boolean isEspecial() {
-        return especial;
-    }
-    public void setEspecial(boolean especial) {
-        this.especial = especial;
-    }
-
     public Cliente(){ //Métodos construtores precisam ser chamados public MetodoConstrutor(), se public void Cliente() ele não é chamado
         System.out.println("Criando um cliente usando o construtor sem parâmetros");
         double aleatorio = Math.random();
@@ -20,40 +17,59 @@ public class Cliente {
         else
             especial = false;
     }
-
     public Cliente(double renda, char sexo){ //Métodos construtores precisam ser chamados public MetodoConstrutor(), se public void Cliente() ele não é chamado
         this(); //this() deve ser na primeira linha e ele chama o construtor anterior, geralmente menos complexo, para evitar duplicidade de código
         System.out.println("Criando um cliente usando o construtor com parâmetros");
         setRenda(renda);
         this.sexo = sexo;
     }
-   
+
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public boolean isEspecial() {
+        return especial;
+    }
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        if(nome.isBlank())
+            System.out.println("Nome é obrigatório.");
+        else
+        this.nome = nome.toUpperCase().trim();
+    }
     public char getSexo() {
         return sexo;
     }
-
     public void setSexo(char sexo) {
-        if(sexo == 'M'|| sexo == 'F')
+        if(sexo == 'M'|| sexo == 'F'|| sexo == 'm'|| sexo == 'f')
             this.sexo = sexo;
         else
-            System.out.println("O sexo deve ser M ou F.");
+            System.out.println("O sexo deve ser M, m, F ou f.");
     }
-
-    private int anoNascimento;
-
     public int getAnoNascimento() {
         return anoNascimento;
     }
-
     public void setAnoNascimento(int anoNascimento) {
         this.anoNascimento = anoNascimento;
     }
-
-
     public double getRenda() {
         return renda;
     }
-
     public void setRenda(double renda){
         if(renda>=0)
             this.renda = renda; //this relaciona ao objeto atual, no caso cliente e este renda refere-se ao argumento setter e não à classe
